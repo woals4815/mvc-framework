f4413fb1f3412c2ebb973c92f14af6712f086918
sexyboyjayden <dev@matchu.vip>
2024-12-19 20:46:28 +0900

    feature: DispatcherServlet 추가

---
+++ src/main/java/next/controller/DispatcherServlet.java
@@ -0,0 +1,17 @@
+package next.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/", loadOnStartup = 0)
+public class DispatcherServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.service(req, resp);
+    }
+}
